fbd660ad8624b902f7fd6a43b1a5ef73c2094278
aman-pati <amanpati4949@gmail.com>
2025-07-07 21:10:22 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,18 @@
+#In this program I am going to do some basic mathematical operations
+#The operations will be + , - , * , /
+a=int(input("Enter a Number: "))
+print()
+b=int(input("Enter a Number: "))
+print()
+add=a+b
+sub=a-b
+mul=a*b
+div=a/b
+print(" Addition: ",add)
+print()
+print(" Subtraction: ",sub)
+print()
+print(" Multiplication: ",mul)
+print()
+print(" Division: ",div)
+print()
---
+++ Task2.py
@@ -0,0 +1,6 @@
+#In this program I am going to take the First and Last name of the User as separate inputs
+#Then I am going to print the full name using concatenation and print a greeting
+firstname=input("Enter your First Name: ")
+lastname=input("Enter your Last Name: ")
+concat=firstname+" "+lastname
+print("Hello,",concat,"! Welcome to Python Programming")
