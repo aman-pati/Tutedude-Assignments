7b065115cfe788762cc3bd63b0d3c87040052a8b
Aman Pati <amanpati4949@gmail.com>
2025-08-31 09:18:15 +0530

    Task10.py

--- Task10.py
+++ Task10.py
@@ -1,10 +1,7 @@
 # program to demonstrate list slicing
-
-numbers = [1,2,3,4,5,6,7,8,9,10]
-
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 first_five = numbers[:5]
-
 rev = first_five[::-1]
-
+print("Original list:", numbers)
 print("First five elements:", first_five)
 print("Reversed list:", rev)
